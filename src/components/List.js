b2057b72a25d56a2884a5b460de347915e9e0ceb
Yair <hello@agzeri.com>
2018-03-13 12:38:21 -0600

    [List.js] Create the Card basic structure.

--- src/components/List.js
+++ src/components/List.js
@@ -8,6 +8,28 @@ class List extends Component {
           <header className='list__header'>
             <h3>Week 3</h3>
           </header>
+          <div className='list__container'>
+            <article>
+              <h4>Day 1 – Monday</h4>
+              <ul>
+                <li>
+                  <i className='fa fa-eye'></i>
+                </li>
+                <li>
+                  <i className='fa fa-bars'></i>
+                </li>
+                <li>
+                  <i className='fa fa-comment-o'></i> 6
+                </li>
+                <li>
+                  <i className='fa fa-paperclip'></i> 2
+                </li>
+                <li>
+                  <i className='fa fa-check-square-o'></i> 0/14
+                </li>
+              </ul>
+            </article>
+          </div>
           <footer>
             <a href='#'>Add a card...</a>
           </footer>
