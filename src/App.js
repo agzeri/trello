02113519ff7eed23b99faf9023a5df355fc98662
Yair <hello@agzeri.com>
2018-03-13 12:20:24 -0600

    Call List.js component in App.js file.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
+import List from './components/List';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>Hey</div>
+      <List />
     );
   }
 }
