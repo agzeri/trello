85467ebae1cd2a52179fcc395158e929d1e060dc
Yair <hello@agzeri.com>
2018-03-13 12:24:26 -0600

    [List.js] Create the header and title element.

--- src/components/List.js
+++ src/components/List.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 class List extends Component {
   render() {
-    return <h2>List</h2>
+    return (
+      <section>
+        <div className='list'>
+          <header className='list__header'>
+            <h3>Week 3</h3>
+          </header>
+        </div>
+      </section>
+    );
   } 
 }
 
